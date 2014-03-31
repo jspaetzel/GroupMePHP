a777668829ba0358ec88259a4a2392ad50103d51
Robert Grant <rgrant@mdi.us.com>
2014-03-31 12:32:53 -0400

    Cleaned up code comments and indentation

--- groupme/members.php
+++ groupme/members.php
@@ -1,42 +1,44 @@
 <?php
 
 class members extends client {
-    
-/*
- * add: Add members to a group
- * 
- * @param string required $id
- * @param array $args
- * 
- * members array — nickname is required. You must use one of the following identifiers: user_id, phone_number, or email.
- *      object
- *          nickname (string) required
- *          user_id (string)
- *          phone_number (string)
- *          email (string)
- *          guid (string)
- * 
- * @return string $return
- * 
- */
+	
+	/*
+	 * add: Add members to a group
+	 * 
+	 * @param string required $id
+	 * @param array $args
+	 * 
+	 * members array — nickname is required. You must use one of the following identifiers: user_id,
+	 *				phone_number, or email.
+	 *      object
+	 *          nickname (string) required
+	 *          user_id (string)
+	 *          phone_number (string)
+	 *          email (string)
+	 *          guid (string)
+	 * 
+	 * @return string $return
+	 * 
+	 */
 	public function add($id, $args){
 		$params = array(
 			'url' => '/groups/' . $id . '/members/add',
 			'method' => 'POST',
-            		'query' => array(),
-            		'payload' => $args
+					'query' => array(),
+					'payload' => $args
 		);
 		
 		return $this->request($params);
 	}
-/*
- * results: Add members to a group
- * 
- * @param string required $group_id
- * @param string required $results_id, This is the guid that's returned from an add request. 
- * 
- * Expects 503 or 404 or 200 response
- */
+
+	/*
+	 * results: Add members to a group
+	 * 
+	 * @param string required $group_id
+	 * @param string required $results_id, This is the guid that's returned from an add request. 
+	 * 
+	 * Expects 503 or 404 or 200 response
+	 */
 	public function results($group_id, $results_id) {
 		$params = array (
 			'url' => '/groups/' . $group_id . '/members/results/' . $results_id,
@@ -46,21 +48,21 @@ class members extends client {
 		
 		return $this->request($params);
 	}
-    
-/*
- * remove: Remove member from a group
- * 
- * @param string required $group_id
- * @param string required $user_id
- * 
- * @return string $return
- * 
- */
+	
+	/*
+	 * remove: Remove member from a group
+	 * 
+	 * @param string required $group_id
+	 * @param string required $user_id
+	 * 
+	 * @return string $return
+	 * 
+	 */
 	public function remove($group_id, $user_id){
 		$params = array(
 			'url' => '/groups/' . $group_id . '/members/' . $user_id . '/remove',
 			'method' => 'POST',
-            		'query' => array(),
+					'query' => array(),
 		);
 		
 		return $this->request($params);
