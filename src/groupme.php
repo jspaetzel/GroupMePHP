5e4907c09e3b6d3faeacd6ac731c81910dfac973
Jordan Hoff <jhoff@twothirds.io>
2019-05-18 13:22:15 -0500

    PHP 7.2+ Compatibility (#14)

--- src/groupme.php
+++ src/groupme.php
@@ -23,7 +23,7 @@ class groupme {
 	public $bots;
 	public $sms;
 	public $leaderboard;
-	
+
 	public function __construct($token = "") {
 		if(isset($token)) {
 			$this->directmessages = new directmessages($token);
@@ -50,7 +50,7 @@ class client {
 	/*
 	 * When creating a new groupmeClient object
 	 * send array with 'access_token'
-	 * 
+	 *
 	 */
 	public function __construct($token) {
 		$this->token = $token;
@@ -61,10 +61,10 @@ class client {
 	 */
 	public function buildQueryString($args = array()) {
 		$args = array_merge($args, array("token" => $this->token));
-		
+
 		$query = "";
 		if (isset($args)) {
-			while (list($key, $val) = each($args)) {
+			foreach($args as $key => $val) {
 				if ( strlen($query) > 0 ) {
 					$query .= "&";
 				} else {
@@ -78,19 +78,19 @@ class client {
 
 	/*
 	 * Overhead function that all requests utilize
-	 */	
+	 */
 	public function request($args) {
 		$c = curl_init();
-		
+
 		curl_setopt($c, CURLOPT_TIMEOUT, 4);
 		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($c, CURLOPT_USERAGENT, 'groupmeClient/0.1');
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($c, CURLOPT_CUSTOMREQUEST, $args['method']);
-		
+
 		$curlurl = $this->url . $args['url'] . $this->buildQueryString($args['query']);
 		curl_setopt($c, CURLOPT_URL, $curlurl );
-		
+
 		if($args['method'] == "POST"){
 			if ( isset($args['payload']) ) {
 				$payload = "";
@@ -101,7 +101,7 @@ class client {
 					$payload = json_encode($args['payload']);
 					curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 				}
-								
+
 				curl_setopt($c, CURLOPT_POSTFIELDS, $payload );
 			}
 		}
